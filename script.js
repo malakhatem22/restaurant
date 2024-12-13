734a1b64c998b7cd7db99ee0ecd9aa156e68b52f
malakhatem22 <mhatem226003@gmail.com>
2024-12-13 19:33:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let food1m = document.getElementById('food1');
+let food2m = document.getElementById('food2');
+let foods = document.getElementById('food');
+
+
+food1m.addEventListener('click',()=>{
+    foods.style.backgroundImage="URL('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm-a-cguGa7K-tyJ-K9p26zPUVVmDE8vRET0pMzI3xUh1KMoWvyjqL6zsEjukrxUcwyeI&usqp=CAU')";
+});
+
+food2m.addEventListener('click',()=>{
+    foods.style.backgroundImage="URL('https://i.pinimg.com/474x/85/59/26/855926ca9c29b9b62f806e534b98ab1d.jpg')";
+});
+
